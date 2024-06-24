bed3e6ebf89b9e64b1506a1d3e1f28f3c7a1a57c
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-06-24 17:01:30 +0530

    Added the Routes

--- Blogzz/routes/blog.js
+++ Blogzz/routes/blog.js
@@ -3,13 +3,19 @@ const router =express.Router()
 
 //importing the controller
 const {dummyLink}=require("../controllers/Likecontroller")
-
-
+const {createComment}=require("../controllers/Commentcontroller")
+const {createPost}=require("../controllers/Postcontroller")
+const {getAllPosts}=require("../controllers/Postcontroller")
+const {likePost,unlikePost}=require("../controllers/Likecontroller")
 
 
 //mapping with the routes path with the controller functions
 router.get("/dummyroute",dummyLink)
-
+router.post("/comments/create",createComment)
+router.post("/posts/create",createPost)
+router.get("/posts",getAllPosts)
+router.post("/likes/like",likePost)
+router.post("likes/unlike",unlikePost)
 
 //exporting the router
 module.exports=router
